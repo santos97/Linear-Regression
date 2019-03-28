400eb7612223fa0d7d728b163555792ef8a0227b
santos97 <39873118+santos97@users.noreply.github.com>
2019-03-28 18:07:09 +0530

    Add files via upload

---
+++ linear.py
@@ -0,0 +1,42 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import pylab as pl
+import numpy as np
+
+df = pd.read_csv("FuelConsumption.csv")
+df.head()
+cdf = df[['ENGINESIZE','CYLINDERS','FUELCONSUMPTION_CITY','FUELCONSUMPTION_HWY','FUELCONSUMPTION_COMB','CO2EMISSIONS']]
+cdf.head(9)
+plt.scatter(cdf.ENGINESIZE, cdf.CO2EMISSIONS,  color='red')
+plt.xlabel("Engine size")
+plt.ylabel("Emission")
+plt.show()
+
+msk = np.random.rand(len(df)) < 0.8
+train = cdf[msk]
+test = cdf[~msk]
+plt.scatter(train.ENGINESIZE, train.CO2EMISSIONS,  color='blue')
+plt.xlabel("Engine size")
+plt.ylabel("Emission")
+plt.show()
+from sklearn import linear_model
+        regr = linear_model.LinearRegression()
+        x = np.asanyarray(train[['ENGINESIZE','CYLINDERS','FUELCONSUMPTION_COMB']])
+        y = np.asanyarray(train[['CO2EMISSIONS']])
+        regr.fit (x, y)
+        # The coefficients
+        print ('Coefficients: ', regr.coef_)
+        print ('Intercept: ',regr.intercept_)
+
+
+y_hat= regr.predict(test[['ENGINESIZE','CYLINDERS','FUELCONSUMPTION_COMB']])
+x = np.asanyarray(test[['ENGINESIZE','CYLINDERS','FUELCONSUMPTION_COMB']])
+y = np.asanyarray(test[['CO2EMISSIONS']])
+print("Residual sum of squares: %.2f"% np.mean((y_hat - y) ** 2))
+         
+print('Variance score: %.2f' % regr.score(x, y))
+
+
+
+#Residual sum of squares: 565.01
+#Variance score: 0.87
